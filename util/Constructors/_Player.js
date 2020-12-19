8f8d6883109390418cba328bbcc7ca379e9505fa
BuildTools <unconfigured@null.spigotmc.org>
2020-12-19 16:09:29 -0500

    Fixed updateNames

--- util/Constructors/_Player.js
+++ util/Constructors/_Player.js
@@ -293,7 +293,7 @@ module.exports = class _Player {
     static updateNames(league) {
         players[league].forEach(json => {
             _MinecaftAPI.getUuid(json.name).then(val2 => {
-                if (val2 == undefined || val2.id != json.uuid) {
+                if ((val2 != undefined && val2.id == undefined) || val2.id != json.uuid) {
                     _MinecaftAPI.getName(json.uuid).then(val => {
                         if (val == null) return;
                         console.log(`${json.name} -> ${val}`);
