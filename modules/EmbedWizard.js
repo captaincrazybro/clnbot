dbb4af566098304fa4f404e88b318dbd128882b3
Levi Wenger <myeyesareonmizar@gmail.com>
2021-01-04 12:51:48 -0500

    bump

--- modules/EmbedWizard.js
+++ modules/EmbedWizard.js
@@ -4,39 +4,43 @@ const Colors = require('../util/Enums/Colors.js');
 
 module.exports.embedWizardMap = new Map();
 
-module.exports.run = (message) => {
-    if(this.embedWizardMap.has(message.author.id)){
-        if(message.content.replace(" ", "").toLowerCase() == "exit" || message.content.replace(" ", "").toLowerCase() == "cancel"){
-            this.embedWizardMap.delete(id);
-            return;
-        }
-
-        let id = message.author.id;
-        let el = this.embedWizardMap.get(id);
+module.exports = class EmbedWizard {
     
-        switch(el.step){
-            case(1):{
-                let color = message.content;
-                el.color = color;
-                el.step++;
-                this.embedWizardMap.set(id, el);
-                
-                return new _NoticeEmbed(Colors.SUCCESS, "You have successfully set the color to " + color + ". Please specify the description (can be multiple lines).")
-                break;
-            }
-            case(2):{
-                let description = message.content;
-                el.description = description;
-                el.step++;
-                this.embedWizardMap.set(id, el);
-
-                return new _NoticeEmbed(Colors.SUCCESS, "You have successfully set the description. Please specify ")
-                break;
-            }
-            default:{
+    static run(message){
+        if(this.embedWizardMap.has(message.author.id)){
+            if(message.content.replace(" ", "").toLowerCase() == "exit" || message.content.replace(" ", "").toLowerCase() == "cancel"){
                 this.embedWizardMap.delete(id);
-                break;
+                return;
+            }
+
+            let id = message.author.id;
+            let el = this.embedWizardMap.get(id);
+        
+            switch(el.step){
+                case(1):{
+                    let color = message.content;
+                    el.color = color;
+                    el.step++;
+                    this.embedWizardMap.set(id, el);
+                    
+                    return new _NoticeEmbed(Colors.SUCCESS, "You have successfully set the color to " + color + ". Please specify the description (can be multiple lines).")
+                    break;
+                }
+                case(2):{
+                    let description = message.content;
+                    el.description = description;
+                    el.step++;
+                    this.embedWizardMap.set(id, el);
+
+                    return new _NoticeEmbed(Colors.SUCCESS, "You have successfully set the description. Please specify ")
+                    break;
+                }
+                default:{
+                    this.embedWizardMap.delete(id);
+                    break;
+                }
             }
         }
     }
+
 }
