1cb2a9b4733ff52f4c899621b31ade19c0e97fdb
captaincrazybro <myeyesareonmizar@gmail.com>
2021-01-04 17:42:24 +0000

    bump

--- util/Constructors/_League.js
+++ util/Constructors/_League.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-var leagues = require("../../storage/leagues.json");
-const fs = require("fs");
-const Discord = require("discord.js")
-const _MinecaftAPI = require("../../util/Constructors/_MinecraftAPI")
-// const {get} = require('lodash');
-
-module.exports = class _League {
-
-    static getLeague(id){
-        if(!leagues[id]) return null;
-        return leagues[id];
-    }
-
-    static setLeague(id, league){
-        leagues[id] = league;
-        fs.writeFile('./storage/leagues.json', JSON.stringify(leagues), (err) => {
-            if(err) console.log(err);
-        })
-    }
-    
-    static resetLeague(id){
-        this.setLeague(id, null);
-    }
-
-    static parseLeague(league){
-        var outcome;
-        if(league.toLowerCase() == "twl" || league.toLowerCase() == "twlleague") outcome = "twl";
-        else if(league.toLowerCase() == "ctfcl" || league.toLowerCase() == "ctfclleague") outcome = "ctfcl";
-        else if(league.toLowerCase() == "dcl" || league.toLowerCase() == "dclleague") outcome = "dcl";
-        else if(league.toLowerCase() == "mbcl" || league.toLowerCase() == "mbclleague") outcome = "mbcl";
-        else if(league.toLowerCase() == "cdcl" || league.toLowerCase() == "cdclleague") outcome = "cdcl";
-        else if(league.toLowerCase() == "decl" || league.toLowerCase() == "declleague") outcome = "decl";
-        else if(league.toLowerCase() == "clt" || league.toLowerCase() == "cltleague") outcome = "clt";
-        else if(league.toLowerCase() == "cwcl" || league.toLowerCase() == "cwclleague") outcome = "cwcl";
-        else if(["cotc","cotcleague"].includes(league.toLowerCase())) outcome = "cotc";
-        else outcome = null;
-        return outcome;
-    }
-
-=======
 var leagues = require("../../storage/leagues.json");
 const fs = require("fs");
 const Discord = require("discord.js")
@@ -78,5 +37,4 @@ module.exports = class _League {
         return outcome;
     }
 
->>>>>>> 14189b824302ffdaa2d729394941ea10d552e880
 }
