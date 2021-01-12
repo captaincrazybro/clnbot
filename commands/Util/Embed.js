d1996f2706047fe67e1470eacab99cc6b9eddd30
Levi Wenger <myeyesareonmizar@gmail.com>
2021-01-12 11:10:01 -0500

    Embed creator

--- commands/Util/Embed.js
+++ commands/Util/Embed.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot,message,args,cmd) => {
 
     if(isNaN(num)) return new _NoticeEmbed(Colors.ERROR, "Invalid channel - Please specify a valid channel (either #channel-name or id)").send(message.channel);
 
-    num = parseInt(num);
+    num = BigInt(num);
 
     let channel = message.guild.channels.cache.get(num);
 
