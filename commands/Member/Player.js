e0063f43bb0a3567b72b3514b55f23cbfb42a9f5
Levi Wenger <myeyesareonmizar@gmail.com>
2021-01-16 23:40:16 -0500

    Discord-Player linking

--- commands/Member/Player.js
+++ commands/Member/Player.js
@@ -36,7 +36,9 @@ module.exports.run = async (bot, message, args, cmd) => {
 
         //let blacklist = _Blacklist.getBlacklist(val.id);
 
-        if(player.discordId != null) let discordUser = await bot.users.fetch(player.discordId)
+        if(player.discordId != null) {
+            let discordUser = await bot.users.fetch(player.discordId)
+        }
 
         let embed = new Discord.MessageEmbed()
             .setColor(Colors.INFO)
